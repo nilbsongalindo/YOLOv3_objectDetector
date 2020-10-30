9fdd4cf9087b2891c9729a94c7cb653428ecbb8e
nilbsongalindo <nilbsonla@gmail.com>
2020-10-29 22:34:55 -0300

    darknet class

--- darknet.py
+++ darknet.py
@@ -17,6 +17,46 @@ class DetectionLayer(nn.Module):
 		self.anchors = anchors
 
 
+class Darknet(nn.Module):
+	def __init__(self, cfgfile):
+		super(Darknet, self).__init__()
+		self.blocks = parse_cfg(cfgfile)
+		self.net_info, self.module_list = create_modules(self.blocks)
+
+	def foward(self, x, CUDA):
+		modules = self.blocks[1:]
+		outputs = {} #cache the outputs for the route layer
+
+		write = 0 
+		for i, module in enumerate(modules):
+			module_type = (module['type'])
+
+			if module_type == 'convolutional' or module_type == 'upsample':
+				x = self.module_list[i](x)
+
+			elif module_type == 'route':
+			layers = module['layers']
+			layers = [int(a) for a in layers] 
+
+			if layers[0] > 0:
+				layers[0] = layers[0] - i
+
+			if len(layers) == 1:
+				x = outputs[i + (layers[0])]
+			else:
+				if layers[1] > 0:
+					layers[1] = layers[1] - i
+
+				map1 = outputs[i + layers[0]]
+				map2 = outputs[i + layers[1]]
+
+				x = torch.cat((map1, map2), 1)
+
+			elif module_type == 'shortcut':
+				from_ = int(module['from'])
+				x = outputs[i-1] + outputs[i+from_]
+
+
 def parse_cfg(cfgfile):
 	"""
 	Takes a configuration file
