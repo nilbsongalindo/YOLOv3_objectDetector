f9077f6a1bd917b6c0ab0a7f9fa7a5ef68232395
nilbsongalindo <nilbsonla@gmail.com>
2020-10-30 17:53:56 -0300

    foward updated

--- darknet.py
+++ darknet.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
+from util import *
 
 
 class EmptyLayer(nn.Module):
@@ -15,7 +16,6 @@ class DetectionLayer(nn.Module):
 	def __init__(self, anchors):
 		super(DetectionLayer, self).__init__()
 		self.anchors = anchors
-
 
 class Darknet(nn.Module):
 	def __init__(self, cfgfile):
@@ -35,27 +35,57 @@ class Darknet(nn.Module):
 				x = self.module_list[i](x)
 
 			elif module_type == 'route':
-			layers = module['layers']
-			layers = [int(a) for a in layers] 
+				layers = module['layers']
+				layers = [int(a) for a in layers] 
 
-			if layers[0] > 0:
-				layers[0] = layers[0] - i
+				if layers[0] > 0:
+					layers[0] = layers[0] - i
 
-			if len(layers) == 1:
-				x = outputs[i + (layers[0])]
-			else:
-				if layers[1] > 0:
-					layers[1] = layers[1] - i
+				if len(layers) == 1:
+					x = outputs[i + (layers[0])]
+				else:
+					if layers[1] > 0:
+						layers[1] = layers[1] - i
 
-				map1 = outputs[i + layers[0]]
-				map2 = outputs[i + layers[1]]
+					map1 = outputs[i + layers[0]]
+					map2 = outputs[i + layers[1]]
 
-				x = torch.cat((map1, map2), 1)
+					x = torch.cat((map1, map2), 1)
 
 			elif module_type == 'shortcut':
 				from_ = int(module['from'])
 				x = outputs[i-1] + outputs[i+from_]
+			
+			elif module_type == 'yolo':
+				anchors = self.module_list[i][0].anchors
+				#Get input dimensions
+				inp_dim = int(self.net_info['height'])
 
+				#Get number of classes
+				num_classes = int(module['classes'])
+
+				#Transform
+				x = x.data
+				x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+
+				if not write:
+					detections = x
+					write = 1
+				else:
+					detections = torch.cat((detections, x), 1)
+
+			outputs[i] = x
+		return detections
+
+
+def get_test_input():
+	img = cv2.imread('dog-cycle-car.png')
+	img = cv2.resize(img, (416, 416))  #Resize to the input dimension
+	img_ = img[:, :, ::-1].transpose((2, 0, 1))  # BGR -> RGB | H X W C -> C X H X W 
+	img_ = img_[np.newaxis, :, :, :]/255.0  #Add a channel at 0 (for batch) | Normalise
+	img_ = torch.from_numpy(img_).float()
+	img_ = Variable(img_)
+	return img_
 
 def parse_cfg(cfgfile):
 	"""
@@ -187,9 +217,9 @@ def create_modules(blocks):
 
 
 
-blocks = parse_cfg("cfg/yolov3.cfg")
-print(create_modules(blocks))
 
 
-
-
+model = Darknet('cfg/yolov3.cfg')
+inp = get_test_input()
+pred = model(inp, False)
+print(pred)
